7dbe956dc3553031145d8ac12d0ee8990faec0db
Abcd <abcd@Zickys-MacBook-Pro.local>
2021-05-04 23:30:14 -0400

    zicky v1 commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Student from './Students'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Student/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
